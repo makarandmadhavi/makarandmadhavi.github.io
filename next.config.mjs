6d67749e42ef955aaddbc28f39809b23765afdbb
Makarand <madhavi.m@northeastern.edu>
2024-03-06 18:42:54 -0800

    fix image and src issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: './',
+    assetPrefix: '/../',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;

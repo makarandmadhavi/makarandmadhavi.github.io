cf538dc9bb0164777620a921cc362d96fe6a8d1d
Makarand <madhavi.m@northeastern.edu>
2024-03-06 17:37:44 -0800

    init ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,116 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <main className="flex min-h-screen flex-col items-center justify-between px-10 py-10 gap-10">
+      <div className="container flex items-center flex-wrap justify-center gap-10">
+        <Image
+          src="/makarand.jpg"
+          alt="Makarand Madhavi"
+          width={250}
+          height={250}
+          className="rounded-full"
+        />
+        <div>
+          <h3 >Hi ! 👋 </h3>
+          <h4 className="card-title mb-0">Makarand here </h4>
+          <br />
+          <p className="secondary-text">A Versatile <span className="primary-accent"> Software Engineer</span></p>
+          <p className="white">Explore my portfolio to see my projects in action!</p>
+        </div>
+      </div>
+      <div className="container flex items-center justify-center gap-10">
+        <h1> <span className="primary-accent">Projects</span>  <span className="white"> &lt;<span className="animated-icon"></span>&gt; </span> </h1>
+      </div>
+      <div className="container flex items-center justify-center gap-10 flex-wrap">
+        <div className="frosted-glass project-item">
+          <a href="https://github.com/makarandmadhavi/Marketting-Campaign-Feature-Engineering-Modeling-Interpretability-report/blob/main/Marketting_Campaign_Feature_Engineering__Modeling_Interpretability_Report.ipynb" target="_blank" rel="noopener noreferrer">
+            <p>Marketing Campaign Response Analysis</p>
+            <div style={{ width: '100%', height: '200px', overflow: 'hidden', position: "relative" }}>
+              <Image
+                src="/mk.png"
+                alt="jupyter notebook"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="top left"
+              />
+            </div>
+          </a>
+        </div>
+        <div className="frosted-glass project-item">
+          <a href="https://github.com/makarandmadhavi/Emotion_Classification" target="_blank" rel="noopener noreferrer">
+            <p>Facial Emotion Classifier</p>
+            <div style={{ width: '100%', height: '200px', overflow: 'hidden', position: "relative" }}>
+              <Image
+                src="/emotion.png"
+                alt="jupyter notebook"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="top left"
+              />
+            </div>
+          </a>
+        </div>
+        <div className="project-item frosted-glass">
+          <a href="https://github.com/makarandmadhavi/NU-Chronicles" target="_blank" rel="noopener noreferrer">
+            <p>NU <br></br> Chronicles</p>
+            <div style={{ width: '100%', height: '200px', overflow: 'hidden', position: "relative" }}>
+              <Image
+                src="/chronicle.png"
+                alt="NU chronicle"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="top left"
+              />
+            </div>
+          </a>
+        </div>
+
+        <div className="project-item frosted-glass">
+          <h5 className="card-title">Cloud Native WebApp</h5>
+          <a href="https://github.com/Makjets/webapp" target="_blank" rel="noopener noreferrer">
+            <p className="secondary-text">WebApp <span className="white"> - Spring Boot Rest API</span></p>
+          </a>
+          <a href="https://github.com/Makjets/iac-pulumi" target="_blank" rel="noopener noreferrer">
+            <p className="secondary-text">IAC <span className="white"> - AWS Infrastructure deployed via Pulumi</span></p>
+          </a>
+          <a href="https://github.com/Makjets/serverless" target="_blank" rel="noopener noreferrer">
+            <p className="secondary-text">serverless computing <span className="white"> - AWS Lambda function</span></p>
+          </a>
+        </div>
+
+        <div className="project-item frosted-glass">
+          <a href="https://github.com/makarandmadhavi/BootKart---E-Commerce" target="_blank" rel="noopener noreferrer">
+            <p>Bootkart - E commerce site</p>
+            <div style={{ width: '100%', height: '200px', overflow: 'hidden', position: "relative" }}>
+              <Image
+                src="/demouser.gif"
+                alt="demo"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="top left"
+              />
+            </div>
+          </a>
+        </div>
+        <div className="project-item frosted-glass">
+          <a href="https://makarandmadhavi.github.io/RollaBallGame/" target="_blank" rel="noopener noreferrer">
+            <p>Roll a Ball Game</p>
+            <p className="white">Created with Unity Engine</p>
+            <div style={{ width: '100%', height: '200px', overflow: 'hidden', position: "relative" }}>
+              <Image
+                src="/rollball.png"
+                alt="Makarand's picture"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
           </a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="text-white text-center p-4">
+        &copy; 2024 Makarand
+      </footer>
     </main>
   );
 }
---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <nav className="sticky top-0 flex items-center justify-between md:px-20 md:py-8 p-5 bg">
+            <Link className='text-xl font-bold primary-accent' href="/">
+                🚀 Mak
+            </Link>
+            <Link className='flex font-bold text-xl secondary-accent' href="mailto:madhavi.m@northeastern.edu">
+                📧 Contact me
+            </Link>
+        </nav>
+    );
+};
+
+export default Navbar;

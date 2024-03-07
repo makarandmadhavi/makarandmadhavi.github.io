fcae3ca8c1fa389c6423a8e459952b57adc74ec4
Makarand <madhavi.m@northeastern.edu>
2024-03-06 18:57:25 -0800

    try basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // assetPrefix: '',
+    basePath: "/nextjs-github-pages",
     images: {
         unoptimized: true,
     },

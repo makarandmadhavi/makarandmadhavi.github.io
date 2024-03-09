1775610bc91c675cccf900831f20393142057fd6
Makarand <madhavi.m@northeastern.edu>
2024-03-08 18:56:26 -0800

    add opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Makarand",
+  metadataBase: new URL('https://makarandmadhavi.me'),
   description: "Makarand Madhavi's personal website",
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
 };
 
 export default function RootLayout({
